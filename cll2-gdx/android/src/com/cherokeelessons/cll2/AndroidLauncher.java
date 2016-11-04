14e7de7fda617882a56701c2da27c06831d88c4c
mjoyner <mjoyner@vbservices.net>
2016-11-04 18:47:03 -0400

    initial project setup

---
+++ cll2-gdx/android/src/com/cherokeelessons/cll2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cherokeelessons.cll2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cherokeelessons.cll2.CLL2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CLL2(), config);
+	}
+}
---
+++ cll2-gdx/desktop/src/com/cherokeelessons/cll2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cherokeelessons.cll2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cherokeelessons.cll2.CLL2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CLL2(), config);
+	}
+}
